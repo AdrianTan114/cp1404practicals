406fe139b9d4b5395742054cc15238650bdd2932
Adrian Tan <adrian.tanhaoyang@my.jcu.edu.au>
2026-09-22 13:00:02 +1000

    Add function to print stars, add docstrings to functions, and changed score type to int in score menu program

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -7,6 +7,7 @@ MENU = """(G)et Valid Score
 
 
 def main():
+    """Display menu."""
     score = get_valid_number(0, 100, "Score: ")
     print(MENU)
     choice = input(">>> ").upper()
@@ -17,7 +18,7 @@ def main():
             result = determine_result(score)
             print(f"Score {score} is {result}")
         elif choice == "S":
-            pass
+            print_stars(score)
         else:
             print("Invalid input")
         print(MENU)
@@ -26,15 +27,16 @@ def main():
 
 
 def get_valid_number(low, high, prompt):
-    """Get a number from the user, ensuring it is within the boundaries low and high"""
-    number = float(input(prompt))
+    """Get a number from the user, ensuring it is within the boundaries low and high."""
+    number = int(input(prompt))
     while number < low or number > high:
         print("Invalid number")
-        number = float(input(prompt))
+        number = int(input(prompt))
     return number
 
 
 def determine_result(score):
+    """Determine result of a score."""
     if score < 0 or score > 100:
         return "Invalid"
     elif score >= 90:
@@ -43,6 +45,11 @@ def determine_result(score):
         return "Passable"
     else:
         return "Bad"
+
+
+def print_stars(number_of_stars):
+    """Print number_of_stars amount of "*"'s."""
+    print("*" * number_of_stars)
 
 
 main()
