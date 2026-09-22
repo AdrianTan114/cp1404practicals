0f0ffff0eb8f409de562610a6fa8890d513ee7e7
Adrian Tan <adrian.tanhaoyang@my.jcu.edu.au>
2026-09-22 12:29:17 +1000

    Add prize message if result is "Excellent"

--- prac_02/score.py
+++ prac_02/score.py
@@ -10,6 +10,8 @@ def main():
     user_score = float(input("Enter score: "))
     user_result = determine_result(user_score)
     print(f"User score {user_score} is {user_result}")
+    if user_result == "Excellent":
+        print("You get a prize!")
     random_score = random.randint(0, 100)
     random_result = determine_result(random_score)
     print(f"Random: {random_score} = {random_result}")
